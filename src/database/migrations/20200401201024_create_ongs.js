27f1e93faf065f1a2ccc827efc9434b4f494dd19
Robinson D. S. Santos <robinsondssantos@gmail.com>
2020-04-10 18:03:35 -0300

    fix table name

--- src/database/migrations/20200401201024_create_ongs.js
+++ src/database/migrations/20200401201024_create_ongs.js
@@ -1,9 +1,9 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('orgs', function (table) {
+  return knex.schema.createTable('ongs', function (table) {
     table.string('id').primary();
     table.string('name').notNullable();
     table.string('email').notNullable();
-    table.string('whatapp').notNullable();
+    table.string('whatsapp').notNullable();
     table.string('city').notNullable();
     table.string('uf', 2).notNullable();
   });
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const OngController = require('./controllers/OngController');
-const IncidentsController = require('./controllers/IncidentsController');
+const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
@@ -13,9 +13,9 @@ routes.post('/ongs', OngController.create);
 
 routes.get('/profile', ProfileController.index);
 
-routes.get('/incidents', IncidentsController.index);
-routes.post('/incidents', IncidentsController.create);
-routes.delete('/incidents/:id', IncidentsController.delete);
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
 
